1b90a125fdd6185b63a5eb9ef1cad7c2df8cfe02
= <rafael.borges2698@gmail.com>
2022-02-12 12:41:26 -0300

    adjusted webpack

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     },
     module: {
         rules: [
-            { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader'] },
+            { test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'style-loader'], exclude: /node_modules/ },
         ],
     },
     optimization: {
